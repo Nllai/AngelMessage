d98ee0f9d14738dca953ad87172c8b321f383340
LZ <lzhang74@binghamton.edu>
2020-11-05 17:06:50 -0800

    update styles

--- js/service.js
+++ js/service.js
@@ -7,4 +7,13 @@ $(document).ready(function(){
        $(this).text('READ MORE');
     }
    });
-   });
+
+    $('#read2').on('click', function () {
+        var text=$('#read2').text();
+        if(text === "READ MORE"){
+        $(this).html('READ LESS');
+        } else{
+        $(this).text('READ MORE');
+    }
+    });
+    });
